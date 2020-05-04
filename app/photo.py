892a6cf8cdaa8978d0e0f513cb8fb70055af4c83
bennettscience <brian.bennett2@gmail.com>
2020-05-04 13:29:44 -0400

    Accept file uploads via API

--- app/photo.py
+++ app/photo.py
@@ -1,7 +1,11 @@
+import os
+import werkzeug
+from werkzeug.utils import secure_filename
 from flask import make_response, jsonify, abort, request, Response
 from flask_restful import Resource, reqparse, fields, marshal
 from app import api, app, db
 from app.models import Photo
+from datetime import datetime
 
 
 # Return all photos with a consistent URL
@@ -11,17 +15,19 @@ photo_fields = {
     "upload_date": fields.Integer(),
     "public": fields.Boolean(),
     "uri": fields.Url("photo"),
+    "file_path": fields.String()
 }
 
 
 class PhotoListAPI(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument("title", type=str, required=True, location="json")
+        self.reqparse.add_argument("title", type=str, required=True, location="form")
         self.reqparse.add_argument(
-            "upload_date", type=int, required=True, location="json"
+            "upload_date", type=str, location="form"
         )
-        self.reqparse.add_argument("public", type=bool, location="json")
+        self.reqparse.add_argument("public", type=bool, location="form")
+        self.reqparse.add_argument("file", type=werkzeug.datastructures.FileStorage, location='files')
         super(PhotoListAPI, self).__init__()
 
     # TODO: Return only public for non-logged in user
@@ -32,19 +38,32 @@ class PhotoListAPI(Resource):
         photos = [marshal(photo, photo_fields) for photo in query]
         return photos, 200
 
-    # TODO: Convert to multipart-form input, not JSON
     def post(self):
         args = self.reqparse.parse_args()
         if not args:
-            abort(jsonify(message=args), 400)
+            abort(400)
 
-        # Write the photo to the DB
-        # TODO: Save the image to the filesystem
-        p = Photo(args["title"], args["upload_date"], args["public"])
-        db.session.add(p)
-        db.session.commit()
+        if 'file' not in request.files:
+            abort(400)
 
-        return {"photo": marshal(p, photo_fields)}, 201
+        upload_file = request.files['file']
+        if upload_file.filename == '':
+            abort(400)
+
+        if upload_file and self.allowed_filename(upload_file.filename):
+            filename = secure_filename(upload_file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            upload_file.save(file_path)
+            p = Photo(args["title"], args["upload_date"], args["public"], file_path)
+            db.session.add(p)
+            db.session.commit()
+
+            return {"photo": marshal(p, photo_fields)}, 201
+
+    @staticmethod
+    def allowed_filename(filename):
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
 class PhotoAPI(Resource):
