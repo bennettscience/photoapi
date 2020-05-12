519e5cecb67751f85c4ebfb3f4dd279e755b087f
bennettscience <brian.bennett2@gmail.com>
2020-05-12 12:37:10 -0400

    add flask-CORS to handle client requests

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
 api = Api(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+CORS(app)
 
 from app import app, photo
